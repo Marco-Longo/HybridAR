b5b55a0463c167e3b4d60ee8f4cbfd9d35f80bcd
Marco-Longo <marco_longo5@hotmail.it>
2020-07-31 17:00:18 +0200

    First commit

---
+++ Assets/GoogleARCore/Source/AugmentedImage/Scripts/ARDeviceManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using GoogleARCore;
+using System;
+
+public class ARDeviceManager : MonoBehaviour
+{
+    private static ARDeviceManager _instance;
+    private float devDistance;
+    private bool tracking;
+
+    private Vector3 devPosition;
+    private Vector3 devOffset;
+    private Vector3 devRotation;
+    private Vector3 devAcceleration;
+
+    public Text devInfoText;
+
+    public static ARDeviceManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        tracking = false;
+        devDistance = -1.0f;
+        devPosition = Vector3.zero;
+        devOffset = Vector3.zero;
+        devRotation = Vector3.zero;
+        devAcceleration = Vector3.zero;
+
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+            Input.gyro.updateInterval = 0.0167f;
+        }
+    }
+
+    private void Update()
+    {
+        // Update current position
+        if (tracking)
+            EvaluatePosition();
+
+        // Update current rotation
+        devRotation = ReadGyroscopeRotation().eulerAngles;
+
+        // Update current acceleration
+        devAcceleration = Input.gyro.userAcceleration;
+
+        devInfoText.text = "Device Info:\n Position: " + devPosition + "\n Rotation: " + devRotation + "\n Acceleration: " + devAcceleration +
+                           "\n Camera Pose: " + Frame.Pose.ToString();
+
+        //IDEA: use Frame.Pose to calculate camera position (tracking seems pretty accurate, but initial position varies every time, 
+        //      since the origin is always set to the point where the device is located when the app is started)
+    }
+
+    private void EvaluatePosition()
+    {
+        if (devOffset == Frame.Pose.position)
+            return;
+
+        //Evaluate the device position, while considering the initial offset
+        devPosition += Frame.Pose.position - devOffset;
+        devOffset = Frame.Pose.position;
+    }
+
+    private Quaternion ReadGyroscopeRotation()
+    {
+        if (Input.gyro.enabled)
+            return new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * Input.gyro.attitude * new Quaternion(0, 0, 1, 0);
+        else
+            return Quaternion.identity;
+    }
+
+    public void SetDistance(float d)
+    {
+        if (devDistance == -1.0f && d > 0.0f)
+        {
+            //Calculate initial position
+            devPosition = new Vector3(0.0f, 0.0f, -d); //We assume that the device is perfectly aligned to the marker (origin of the system)
+            devOffset = Frame.Pose.position;
+            tracking = true;
+        }
+
+        devDistance = d;
+    }
+
+    /*
+    public void SetPosition(float x, float y, float z)
+    {
+        devPosition.x = x; // The alignment error between the device and the marker centres (horizontal axis)
+        devPosition.y = y; // The height difference between the device and the marker centres (vertical axis)
+        devPosition.z = z; // The linear distance between the device and the marker (forward axis)
+    }
+    */
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/GoogleARCore/Source/AugmentedImage/Scripts/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadARCore()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void LoadAccelArray()
+    {
+        //SceneManager.LoadScene(2);
+        ShowAndroidToastMessage("Load AccelArray Scene");
+    }
+    
+    public void LoadCumAccel()
+    {
+        //SceneManager.LoadScene(3);
+        ShowAndroidToastMessage("Load CumAccel Scene");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    private void ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
